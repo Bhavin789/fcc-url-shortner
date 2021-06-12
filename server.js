be7524d18dff02067e326245c506c728cf3ab946
Bhavin Ag <bhavib.789@gmail.com>
2021-06-12 07:08:18 +0000

    feat: add a dns check for an example url, wip

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns');
 const app = express();
 
 // Basic Configuration
@@ -10,15 +11,29 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl", (req, res) => {
+  dns.lookup('example.com', (err, address, family) => {
+    if(err !== null){
+      return res.json({ error: 'invalid url' });
+    }
+    /**
+     * if the url is correct, save it to the db with a random * number
+     */
+    console.log('address: %j family: IPv%s', address, family)
+    res.json({ success: true });
+  }
+  );
+})
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
